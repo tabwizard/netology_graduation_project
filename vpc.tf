0b7d117424400230baaaed818c93effa466054b7
Andrey Pirozhkov <tabwizard@gmail.com>
2022-05-17 10:32:28 +0700

    first commit

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+resource "yandex_vpc_network" "pirozhkov-netology-vpc" {
+  name = "pirozhkov-netology-vpc"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  count          = 2
+  name           = "subnet-${var.zones[count.index]}"
+  zone           = var.zones[count.index]
+  network_id     = yandex_vpc_network.pirozhkov-netology-vpc.id
+  v4_cidr_blocks = [var.cidr[count.index]]
+}
